8054971d3c44cd2716d0cccb8ccb9247c508d2a0
Nicolas Bellaiche <nicolas.bellaiche@ign.fr>
2026-09-08 16:11:49 +0200

    example

--- example/launch_example.sh
+++ example/launch_example.sh
@@ -1,5 +1,5 @@
 
 export  GIT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 export GSPLAT_PROFILE="balanced"
-
+export CONFIG_SH=$GIT_ROOT/config/config.sh
 bash $GIT_ROOT/environment/ign.slurm/launch.sh --dataset_dir $GIT_ROOT/example --output_dir $GIT_ROOT/example/output
