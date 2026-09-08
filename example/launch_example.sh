b8c745b9e426ae685555d7b980675b2f864ea9c4
Nicolas Bellaiche <nicolas.bellaiche@ign.fr>
2026-09-08 16:28:44 +0200

    example

--- example/launch_example.sh
+++ example/launch_example.sh
@@ -1,8 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
 
-export  GIT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-export GSPLAT_PROFILE="balanced"
-export CONFIG_SH=$GIT_ROOT/config/config.sh
-export OUTPUT_DIR=$GIT_ROOT/example/output
+GIT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+GSPLAT_PROFILE="balanced"
+OUTPUT_DIR=$GIT_ROOT/example/output
 
-bash $GIT_ROOT/environment/ign.slurm/launch.sh --dataset_dir $GIT_ROOT/example --output_dir $OUTPUT_DIR
-
+echo  GIT_ROOT=$GIT_ROOT GSPLAT_PROFILE=$GSPLAT_PROFILE bash $GIT_ROOT/environment/ign.slurm/launch.sh --dataset_dir $GIT_ROOT/example --output_dir $OUTPUT_DIR
+GIT_ROOT=$GIT_ROOT GSPLAT_PROFILE=$GSPLAT_PROFILE bash $GIT_ROOT/environment/ign.slurm/launch.sh --dataset_dir $GIT_ROOT/example --output_dir $OUTPUT_DIR
