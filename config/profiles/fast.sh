540d0ec5bcd689145968ab7b9d7229bd04b97c59
Nicolas Bellaiche <nicolas.bellaiche@ign.fr>
2026-09-17 16:31:07 +0200

    example

--- config/profiles/fast.sh
+++ config/profiles/fast.sh
@@ -1,5 +1,5 @@
 ########################################
-# PROFILE: force_split (agressif)
+# PROFILE: fast_moderate
 ########################################
 
 TRAINING_PROFILE="gpu/fast"
@@ -13,7 +13,7 @@ TRAIN_VIS_MODE="tensorboard"
 # IMAGE / PREPROCESSING
 ########################################
 
-CAMERA_RES_SCALE_FACTOR=0.75
+CAMERA_RES_SCALE_FACTOR=0.5
 MAX_RES=1024
 NUM_DOWNSCALES=1
 SKIP_IMAGE_PROCESSING=true
@@ -23,26 +23,50 @@ SKIP_IMAGE_PROCESSING=true
 ########################################
 
 MAX_ITER=6000
-STOP_SPLIT_AT=5500
+STOP_SPLIT_AT=5200
 TRAIN_RAYS_PER_BATCH=768
 
-NUM_NERF_SAMPLES_PER_RAY=32
-NUM_PROPOSAL_SAMPLES_PER_RAY="64 32"
+NUM_NERF_SAMPLES_PER_RAY=16
+NUM_PROPOSAL_SAMPLES_PER_RAY="32 16"
 
 ########################################
-# GAUSSIAN SPLATTING (FORCE DENSIFY)
+# GAUSSIAN SPLATTING
+# DENSIFICATION MODEREE
 ########################################
 
-# déclenche split très facilement
-DENSIFY_GRAD_THRESH=0.00003
-SPLIT_SCREEN_SIZE=0.008
-REFINE_EVERY=50
+# Intermédiaire entre :
+#   fast     = 0.00003
+#   balanced = 0.00045
+#
+# Plus la valeur est élevée, moins il y a de Gaussians candidats.
+DENSIFY_GRAD_THRESH=0.00015
 
-# évite de tuer les splats trop tôt
-CULL_ALPHA_THRESH=0.005
-CULL_SCALE_THRESH=0.05
-CULL_SCREEN_SIZE=0.05
-RESET_ALPHA_EVERY=200
+# Intermédiaire entre :
+#   fast     = 0.008
+#   balanced = 0.02
+SPLIT_SCREEN_SIZE=0.015
+
+# Densification toutes les 100 itérations au lieu de 50.
+# Cela laisse aux Gaussians le temps de converger entre deux raffinements.
+REFINE_EVERY=100
+
+########################################
+# CULLING / CLEANING
+########################################
+
+# Plus strict que fast, mais beaucoup moins brutal que balanced=0.12.
+CULL_ALPHA_THRESH=0.03
+
+# Supprime les Gaussians excessivement grands.
+# Valeur intermédiaire entre fast=0.05 et balanced=0.5.
+CULL_SCALE_THRESH=0.20
+
+# Nettoyage screen-space modéré.
+CULL_SCREEN_SIZE=0.12
+
+# Le compteur est lié aux cycles de raffinement.
+# Avec REFINE_EVERY=100, 50 correspond à un reset vers 5000 steps.
+RESET_ALPHA_EVERY=50
 
 ########################################
 # QUALITY / REGULARIZATION
@@ -50,12 +74,15 @@ RESET_ALPHA_EVERY=200
 
 USE_BILATERAL_GRID=true
 USE_SCALE_REGULARIZATION=false
-MAX_GAUSS_RATIO=8.0
-SSIM_LAMBDA=0.20
+
+# Limite plus basse que fast=8, mais moins restrictive que balanced=4.
+MAX_GAUSS_RATIO=5.0
+
+SSIM_LAMBDA=0.22
 
 ########################################
 # EXPORT
 ########################################
 
-EXPORT_NUM_POINTS=1200000
+EXPORT_NUM_POINTS=800000
 EXPORT_DOWNSAMPLE=1
